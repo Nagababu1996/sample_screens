9b6a6641c69f194f30cbc028a319204fd92e8cef
unknown <nagababu.self@gmail.com>
2024-08-14 09:41:32 +0530

    first

---
+++ Tuto1/src/test/java/Tuto1/Tuto1/Data1.java
@@ -0,0 +1,51 @@
+package Tuto1.Tuto1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Data1 {
+WebDriver driver;
+	@BeforeTest
+	public void start() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.naukri.com/mnjuser/homepage");
+
+
+	}
+	
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void case1() {
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//span[text()=\"Search jobs here\"]")).click();
+		driver.findElement(By.xpath("//input[@placeholder=\"Enter keyword / designation / companies\"]")).sendKeys("QA Testing");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//b[text()=\"QA testing\"]")).click();
+		driver.findElement(By.id("experienceDD")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//li[@value=\"a0\"]")).click();
+		driver.findElement(By.xpath("//input[@placeholder=\"Enter location\"]")).sendKeys("Bangalore");
+		driver.findElement(By.xpath("//b[text()=\"Bengalur\"]")).click();
+		driver.findElement(By.xpath("//span[text()=\"Search\"]")).click();
+		
+
+	}
+	
+	@AfterTest
+	public void teardown() {
+		
+		driver.close();
+		
+
+	}
+}
